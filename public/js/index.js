144263d2880ea24e32c2fbb7502bf2fb7a2b397e
Zain Mir <zainnaeemk10@gmail.com>
2021-01-12 18:31:58 +0500

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+    title =$('div[class="title_wrapper"]>h1').text().trim() //getting title from the website
+    rating=$('div[class="ratingValue"]').text().trim() //getting the rating
+    summary=$('div[class="summary_text"]').text().trim()
